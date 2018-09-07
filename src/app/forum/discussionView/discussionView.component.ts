033ffcbd94ae1d38b1ea5b1ab1ee596aced77c19
Mikyas <mikidam63@gmail.com>
2018-09-07 12:41:51 +0300

    small fix

--- src/app/forum/discussionView/discussionView.component.ts
+++ src/app/forum/discussionView/discussionView.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { FormGroup, Validators, FormBuilder, FormControl } from "@angular/forms";
 import { ForumService } from "../forum.service";
 import { ActivatedRoute, Router } from "@angular/router";
