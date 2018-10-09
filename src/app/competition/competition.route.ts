2fd4fe2612b9464c1e3fadbc69c8dc34fc7aa5d2
Mikyas <mikidam63@gmail.com>
2018-10-09 10:08:13 +0300

    added competition project component

--- src/app/competition/competition.route.ts
+++ src/app/competition/competition.route.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { CompetitionListComponent } from './competitionList/competitionList.component';
+import { CompetitionProjectsComponent } from './competitionProjects/competitionProjects.component';
 
 export const CompetitionRoutes: Routes = [
 	{
 		path: "competition",
 		children: [
-			{path: '', component: CompetitionListComponent}
+			{path: '', component: CompetitionListComponent},
+			{path: ':competitionId', component: CompetitionProjectsComponent}
 		]
 	}
 ];
