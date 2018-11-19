3ac550dfe20acf9b8afda6901e19a3866b62d628
Kaleab Yitbarek <kalzbest@gmail.com>
2018-11-19 11:55:51 +0300

    google analytics integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import "rxjs/add/operator/distinctUntilChanged";
+
+declare var gtag: Function;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(public router: Router) {}
 
+  ngOnInit(): void {
+    this.router.events
+      .distinctUntilChanged((previous: any, current: any) => {
+        if (current instanceof NavigationEnd) {
+          return previous.url === current.url;
+        }
+        return true;
+      })
+      .subscribe((x: any) => {
+        gtag("config", "UA-129320736-1", { page_path: x.url });
+      });
+  }
 }
