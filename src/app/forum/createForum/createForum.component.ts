7d1f8f9741f13a852d0eb9358bc800b8d3a8e7f0
Kaleab Yitbarek <kalzbest@gmail.com>
2018-12-18 17:17:50 +0300

    add forum description

--- src/app/forum/createForum/createForum.component.ts
+++ src/app/forum/createForum/createForum.component.ts
@@ -1,66 +1,83 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
-import { ForumService } from '../forum.service';
-import { AuthService } from '../../Auth/services/auth.service';
-import { SharedService } from '../../shared/services/shared.service';
-import {Router} from '@angular/router';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import {
+  FormGroup,
+  Validators,
+  FormBuilder,
+  FormControl
+} from "@angular/forms";
+import { ForumService } from "../forum.service";
+import { AuthService } from "../../Auth/services/auth.service";
+import { SharedService } from "../../shared/services/shared.service";
+import { Router } from "@angular/router";
 
 @Component({
-    selector: 'app-forum-create',
-    templateUrl: './createForum.component.html',
-    styleUrls: ['./createForum.component.css']
+  selector: "app-forum-create",
+  templateUrl: "./createForum.component.html",
+  styleUrls: ["./createForum.component.css"]
 })
-
 export class CreateForumComponent {
+  @Input() categories;
+  @Output() created = new EventEmitter();
+  public forum = {
+    userAccountId: 0,
+    created: new Date(),
+    name: "",
+    slung: "",
+    categoryId: "",
+    private: "",
+    description: ""
+  };
+  public forumForm: FormGroup;
 
-    @Input() categories;
-    @Output() created = new EventEmitter();
-    public forum = { userAccountId: 0, created: new Date(), name: '', slung: '', categoryId: '', private: '' };
-    public forumForm: FormGroup;
+  constructor(
+    public service: ForumService,
+    public authService: AuthService,
+    public sharedService: SharedService,
+    public router: Router
+  ) {
+    this.forumForm = new FormGroup({
+      name: new FormControl("", Validators.required),
+      category: new FormControl("", Validators.required),
+      type: new FormControl("", Validators.required),
+      description: new FormControl("", Validators.required)
+    });
+  }
 
-    constructor (public service: ForumService, public authService: AuthService,
-                 public sharedService: SharedService, public router: Router) {
-        this.forumForm = new FormGroup({
-            name: new FormControl('', Validators.required),
-            category: new FormControl('', Validators.required),
-            type: new FormControl('', Validators.required),
-        });
-    }
-
-    createForum() {
-      if (this.forumForm.valid) {
-        this.forum.created = new Date();
-        this.forum.slung = this.forum.name.trim().split(' ').join('-');
-        this.service.createForum(this.forum).subscribe(
-          res => {
-            const forumId = res.id;
-            this.created.emit();
-            const userId = this.authService.getUserId();
-            if (userId) {
-              const member = {
-                forumId: forumId,
-                userId: userId
-              };
-              this.service.addMember(member).subscribe(
-                res2 => {
-                  console.log(res2);
-                }
-              );
-            }
-          },
-          err => {
-            if (err.status = 422) {
-              this.sharedService.addToast('', 'Error occured!', 'error');
-            }
+  createForum() {
+    if (this.forumForm.valid) {
+      this.forum.created = new Date();
+      this.forum.slung = this.forum.name
+        .trim()
+        .split(" ")
+        .join("-");
+      this.service.createForum(this.forum).subscribe(
+        res => {
+          const forumId = res.id;
+          this.created.emit();
+          const userId = this.authService.getUserId();
+          if (userId) {
+            const member = {
+              forumId: forumId,
+              userId: userId
+            };
+            this.service.addMember(member).subscribe(res2 => {
+              console.log(res2);
+            });
           }
-        );
-      } else {
-        this.markFormGroupTouched(this.forumForm);
-      }
+        },
+        err => {
+          if ((err.status = 422)) {
+            this.sharedService.addToast("", "Error occured!", "error");
+          }
+        }
+      );
+    } else {
+      this.markFormGroupTouched(this.forumForm);
     }
+  }
 
   onSignIn() {
-      this.router.navigate(['login']);
+    this.router.navigate(["login"]);
   }
 
   /**
