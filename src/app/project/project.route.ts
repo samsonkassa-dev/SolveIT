2ed98545636d7ac1ce9e8a97a066833f95bc7fce
Mikyas <mikidam63@gmail.com>
2019-07-22 15:23:44 +0300

    recommendation component view -- not completed

--- src/app/project/project.route.ts
+++ src/app/project/project.route.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProjectContainerComponent } from './projectContainer.component';
 import { ProjectViewComponent } from './projectView/projectView.component';
+import { RecommendationsComponent } from './recommendations/recommendations.component';
 
 export const ProjectRoutes: Routes = [
   {
@@ -8,6 +9,12 @@ export const ProjectRoutes: Routes = [
     children: [
       {path: '', pathMatch: 'full', component: ProjectContainerComponent},
       {path: ':id', component: ProjectViewComponent}
+    ]
+  },
+  {
+    path: 'projects',
+    children: [
+      {path: '', component: RecommendationsComponent}
     ]
   }
 ];
