48eff5d59fa7dd7c89aa47b3dcf203c92dd11176
Mikyas <mikidam63@gmail.com>
2019-08-13 15:32:21 +0300

    fetch user bookmarks

--- common/models/project-bookmark.js
+++ common/models/project-bookmark.js
@@ -2,4 +2,34 @@
 
 module.exports = function(Projectbookmark) {
 
+    // fetch bookmarked projects
+	Projectbookmark.fetchBookmarks = function(req, cb) {
+        // fetchBookmarks code
+        const userId = req.accessToken.userId;
+        console.log(userId);
+		Projectbookmark.find({ where: { userId: userId }, include: ['solveitproject'] }, function(err, projects) {
+			cb(null, projects);
+		});
+
+	}
+  
+	Projectbookmark.remoteMethod('fetchBookmarks', {
+		description: "fetch bookmarks of a user",
+		accepts: [
+            {
+              arg: 'req', 
+              type: 'object', 
+              'http': {source: 'req'}
+            }
+        ],
+		http: {
+		  verb: "get",
+		  path: "/userBookmarks"
+		},
+		returns: {
+		  type: "object",
+		  root: true
+		}
+	});
+
 };
