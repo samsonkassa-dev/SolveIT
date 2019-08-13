a6d990b25bbdb5105eac006ca86f14314b583072
Mikyas <mikidam63@gmail.com>
2019-08-13 15:33:55 +0300

    search remote method for project

--- common/models/solveit-project.js
+++ common/models/solveit-project.js
@@ -7,4 +7,26 @@ module.exports = function (Solveitproject) {
   Solveitproject.disableRemoteMethod("destroyById", true);
   Solveitproject.disableRemoteMethod("removeById", true);
 
+  // search projects
+  Solveitproject.search = function(keyword, cb) {
+      // search code
+  }
+
+  Solveitproject.remoteMethod('search', {
+      description: "Search projects",
+      accepts: {
+        arg: "keyword",
+        type: "string",
+        require: true
+      },
+      http: {
+        verb: "post",
+        path: "/search"
+      },
+      returns: {
+        type: "object",
+        root: true
+      }
+  });
+
 };
