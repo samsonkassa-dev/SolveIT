743bb76d35f193430264d283a3d80e53520f2f73
Kaleab Yitbarek <kalzbest@gmail.com>
2019-08-15 11:59:33 +0300

    :bug: simple bug fix

--- common/models/solveit-project.js
+++ common/models/solveit-project.js
@@ -1,32 +1,30 @@
 'use strict';
 
-module.exports = function (Solveitproject) {
-
+module.exports = function(Solveitproject) {
   //  disable delete end point
-  Solveitproject.disableRemoteMethod("deleteById", true);
-  Solveitproject.disableRemoteMethod("destroyById", true);
-  Solveitproject.disableRemoteMethod("removeById", true);
+  Solveitproject.disableRemoteMethod('deleteById', true);
+  Solveitproject.disableRemoteMethod('destroyById', true)s;
+  Solveitproject.disableRemoteMethod('removeById', true);
 
   // search projects
   Solveitproject.search = function(keyword, cb) {
-      // search code
-  }
+    // search code
+  };
 
   Solveitproject.remoteMethod('search', {
-      description: "Search projects",
-      accepts: {
-        arg: "keyword",
-        type: "string",
-        require: true
-      },
-      http: {
-        verb: "post",
-        path: "/search"
-      },
-      returns: {
-        type: "object",
-        root: true
-      }
+    description: 'Search projects',
+    accepts: {
+      arg: 'keyword',
+      type: 'string',
+      require: true,
+    },
+    http: {
+      verb: 'post',
+      path: '/search',
+    },
+    returns: {
+      type: 'object',
+      root: true,
+    },
   });
-
 };
