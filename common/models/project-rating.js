0099da6da45ad481b53d63056b9a235ba8bfd9e9
Mikyas <mikidam63@gmail.com>
2019-08-17 11:34:38 +0300

    fetch top rated fix

--- common/models/project-rating.js
+++ common/models/project-rating.js
@@ -17,12 +17,37 @@ module.exports = function(Projectrating) {
 	});
 
 	// fetch top tated projects
-	Projectrating.fetchTopRated = function(cb) {
+	Projectrating.fetchTopRated = async function() {
 		// fetchTopRated code
-		Projectrating.find({ where: { rating: {gt: 3.5} }, limit: 10, include: ['project'] }, function(err, projects) {
-				cb(null, projects);
-		});
 
+		// to make sum of an attribute from array of objects
+		Array.prototype.sum = function (prop) {
+			var total = 0
+			for ( var i = 0, _len = this.length; i < _len; i++ ) {
+					total += this[i][prop]
+			}
+			return total
+		}
+
+		function getUniqueValuesOfKey(array, key){
+			return array.reduce(function(carry, item){
+				if(item[key] && !~carry.indexOf(item[key])) carry.push(item[key]);
+				return carry;
+			}, []);
+		}
+
+		let projects = await Projectrating.find({include: ['project'] });
+		let projectIds = getUniqueValuesOfKey(projects, 'projectId');
+		let response = [];
+		for (const id of projectIds) {
+			let temp = projects.filter(item => {
+				return item.projectId == id;
+			})
+			let rating = temp.sum('rating')/temp.length;
+			response.push({rating: rating, project: temp[0].project});
+		}
+
+		return response;
 	}
   
 	Projectrating.remoteMethod('fetchTopRated', {
