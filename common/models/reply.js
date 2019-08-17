395aeb36cb87bd30e1ad3812dd80c10a2c125b9f
Kaleab Yitbarek <kalzbest@gmail.com>
2019-08-17 11:14:36 +0300

    :bug: fix bugs on adding reply and comment

--- common/models/reply.js
+++ common/models/reply.js
@@ -1,34 +1,39 @@
 'use strict';
 
 module.exports = function(Reply) {
+  //  disable delete end point
+  Reply.disableRemoteMethod('deleteById', true);
+  Reply.disableRemoteMethod('destroyById', true);
+  Reply.disableRemoteMethod('removeById', true);
 
-    //  disable delete end point
-  Reply.disableRemoteMethod("deleteById", true);
-  Reply.disableRemoteMethod("destroyById", true);
-  Reply.disableRemoteMethod("removeById", true);
-
-  Reply.afterRemote('create', function (context, unused, next) {
+  Reply.afterRemote('create', function(context, unused, next) {
     var replyCount = 0;
-    const discussionComment = Reply.app.models.Solveitdiscussioncomment;
+    const discussionComment = Reply.app.models['SolveIT-Discussion-Comment'];
 
-    discussionComment.find({
-      where: {
-        id: context.args.data.commentId
+    discussionComment.find(
+      {
+        where: {
+          id: context.args.data.commentId,
+        },
+      },
+      function(err, comment) {
+        replyCount = comment[0].replyCount + 1;
+        discussionComment.updateAll(
+          {
+            id: context.args.data.commentId,
+          },
+          {
+            replyCount: replyCount,
+          },
+          function(err, count) {
+            if (err) {
+              console.error(err);
+              next(err);
+            }
+            next();
+          }
+        );
       }
-    }, function (err, comment) {
-      replyCount = comment[0].replyCount + 1;
-      discussionComment.updateAll({
-        id: context.args.data.commentId
-      }, {
-        replyCount: replyCount
-      }, function (err, count) {
-        if (err) {
-          console.error(err);
-          next(err);
-        }
-        next();
-      })
-    })
+    );
   });
-
 };
