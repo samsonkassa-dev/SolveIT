1287965ac3d0dd6e5b5f5937dd9644bb376e75da
Kaleab Yitbarek <kalzbest@gmail.com>
2019-08-17 12:00:30 +0300

    :bug: bug fix on fetching most viewed projects

--- common/models/project-rating.js
+++ common/models/project-rating.js
@@ -1,90 +1,100 @@
 'use strict';
 
 module.exports = function(Projectrating) {
-
   Projectrating.beforeRemote('create', function(context, unused, next) {
-		let data = context.args.data;
-		Projectrating.find({where: {ratorId: context.args.data.ratorId, projectId: context.args.data.projectId}}, function(err, account) {
-			if (account.length > 0) {
-			Projectrating.updateAll({ id: account[0].id }, context.args.data, function(err, old) {
-	
-			});
-				next("previous rating updated!")
-			} else {
-				next();	
-			}
-		});
-	});
+    let data = context.args.data;
+    Projectrating.find(
+      {
+        where: {
+          ratorId: context.args.data.ratorId,
+          projectId: context.args.data.projectId,
+        },
+      },
+      function(err, account) {
+        if (account.length > 0) {
+          Projectrating.updateAll(
+            {id: account[0].id},
+            context.args.data,
+            function(err, old) {}
+          );
+          next('previous rating updated!');
+        } else {
+          next();
+        }
+      }
+    );
+  });
 
-	// fetch top tated projects
-	Projectrating.fetchTopRated = async function() {
-		// fetchTopRated code
+  // fetch top tated projects
+  Projectrating.fetchTopRated = async function() {
+    // fetchTopRated code
 
-		// to make sum of an attribute from array of objects
-		Array.prototype.sum = function (prop) {
-			var total = 0
-			for ( var i = 0, _len = this.length; i < _len; i++ ) {
-					total += this[i][prop]
-			}
-			return total
-		}
+    // to make sum of an attribute from array of objects
+    Array.prototype.sum = function(prop) {
+      var total = 0;
+      for (var i = 0, _len = this.length; i < _len; i++) {
+        total += this[i][prop];
+      }
+      return total;
+    };
 
-		function getUniqueValuesOfKey(array, key){
-			return array.reduce(function(carry, item){
-				if(item[key] && !~carry.indexOf(item[key])) carry.push(item[key]);
-				return carry;
-			}, []);
-		}
+    function getUniqueValuesOfKey(array, key) {
+      return array.reduce(function(carry, item) {
+        if (item[key] && !~carry.indexOf(item[key])) carry.push(item[key]);
+        return carry;
+      }, []);
+    }
 
-		let projects = await Projectrating.find({include: ['project'] });
-		let projectIds = getUniqueValuesOfKey(projects, 'projectId');
-		let response = [];
-		for (const id of projectIds) {
-			let temp = projects.filter(item => {
-				return item.projectId == id;
-			})
-			let rating = temp.sum('rating')/temp.length;
-			response.push({rating: rating, project: temp[0].project});
-		}
+    let projects = await Projectrating.find({include: ['project']});
+    let projectIds = getUniqueValuesOfKey(projects, 'projectId');
+    let response = [];
+    for (const id of projectIds) {
+      let temp = projects.filter(item => {
+        return item.projectId == id;
+      });
+      let rating = temp.sum('rating') / temp.length;
+      response.push({rating: rating, project: temp[0].toJSON().project});
+    }
 
-		return response;
-	}
-  
-	Projectrating.remoteMethod('fetchTopRated', {
-		description: "fetch top rated projects",
-		http: {
-		  verb: "get",
-		  path: "/topRated"
-		},
-		returns: {
-		  type: "object",
-		  root: true
-		}
-	});
+    return response;
+  };
 
-	// fetch project ratings
-	Projectrating.fetchProjectRatings = function(projectId, cb) {
-		// fetchProjectRatings code
-		Projectrating.find({ where: { projectId: projectId }, include: ['project', 'rator'] }, function(err, projects) {
-				cb(null, projects);
-		});
+  Projectrating.remoteMethod('fetchTopRated', {
+    description: 'fetch top rated projects',
+    http: {
+      verb: 'get',
+      path: '/topRated',
+    },
+    returns: {
+      type: 'object',
+      root: true,
+    },
+  });
 
-	}
-  
-	Projectrating.remoteMethod('fetchProjectRatings', {
-		description: "fetch project ratings",
-		accepts: {
-			arg: 'projectId',
-			type: 'string'
-		},
-		http: {
-		  verb: "get",
-		  path: "/:projectId/ratings"
-		},
-		returns: {
-		  type: "object",
-		  root: true
-		}
-	});
+  // fetch project ratings
+  Projectrating.fetchProjectRatings = function(projectId, cb) {
+    // fetchProjectRatings code
+    Projectrating.find(
+      {where: {projectId: projectId}, include: ['project', 'rator']},
+      function(err, projects) {
+        cb(null, projects);
+      }
+    );
+  };
 
+  Projectrating.remoteMethod('fetchProjectRatings', {
+    description: 'fetch project ratings',
+    accepts: {
+      arg: 'projectId',
+      type: 'string',
+    },
+    http: {
+      verb: 'get',
+      path: '/:projectId/ratings',
+    },
+    returns: {
+      type: 'object',
+      root: true,
+    },
+  });
 };
--- common/models/project-view.js
+++ common/models/project-view.js
@@ -2,41 +2,36 @@
 
 module.exports = function(Projectview) {
   // register project view
-  Projectview.registerView = async (req, projectViewObject) => {
+  Projectview.registerView = async function(req, projectViewObject) {
     const userId = req.accessToken.userId;
-
-    // register project view
-    Projectview.registerView = async function(req, projectViewObject) {
-      const userId = req.accessToken.userId;
-      console.log(projectViewObject.projectViewObject.userId);
-      if (userId == projectViewObject.projectViewObject.userId) {
-        let viewObjectsOfUser = await Projectview.find({
-          where: {
-            userId: userId,
-            projectId: projectViewObject.projectViewObject.projectId,
-          },
-        });
-        if (viewObjectsOfUser.length > 0) {
-          let previousTime = new Date(viewObjectsOfUser[0].lastSeen);
-          if (new Date().getTime() - previousTime.getTime() >= 28800) {
-            let response = await viewObjectsOfUser[0].updateAttributes({
-              lastSeen: new Date(),
-              viewCount: viewObjectsOfUser[0].viewCount + 1,
-            });
-            return response;
-          } else {
-            return viewObjectsOfUser[0];
-          }
-        } else {
-          let viewObject = projectViewObject.projectViewObject;
-          viewObject = {...viewObject, viewCount: 1};
-          let response = await Projectview.create(viewObject);
+    console.log(projectViewObject, '___-----_______');
+    if (userId == projectViewObject.projectViewObject.userId) {
+      let viewObjectsOfUser = await Projectview.find({
+        where: {
+          userId: userId,
+          projectId: projectViewObject.projectViewObject.projectId,
+        },
+      });
+      if (viewObjectsOfUser.length > 0) {
+        let previousTime = new Date(viewObjectsOfUser[0].lastSeen);
+        if (new Date().getTime() - previousTime.getTime() >= 28800) {
+          let response = await viewObjectsOfUser[0].updateAttributes({
+            lastSeen: new Date(),
+            viewCount: viewObjectsOfUser[0].viewCount + 1,
+          });
           return response;
+        } else {
+          return viewObjectsOfUser[0];
         }
       } else {
-        return {};
+        let viewObject = projectViewObject.projectViewObject;
+        viewObject = {...viewObject, viewCount: 1};
+        let response = await Projectview.create(viewObject);
+        return response;
       }
-    };
+    } else {
+      return {};
+    }
   };
 
   Projectview.remoteMethod('registerView', {
@@ -90,7 +85,10 @@ module.exports = function(Projectview) {
         return item.projectId == id;
       });
       let view = temp.sum('viewCount');
-      response.push({viewCount: view, solveitproject: temp[0].solveitproject});
+      response.push({
+        viewCount: view,
+        solveitproject: temp[0].toJSON().solveitproject,
+      });
     }
 
     return response;
@@ -99,35 +97,12 @@ module.exports = function(Projectview) {
   Projectview.remoteMethod('fetchMostViewed', {
     description: 'fetch most viewed projects',
     http: {
-      verb: 'post',
-      path: '/registerView',
+      verb: 'get',
+      path: '/mostViewed',
     },
     returns: {
       type: 'object',
       root: true,
     },
   });
-
-  // fetch most viewed projects
-  Projectview.fetchMostViewed = function(cb) {
-    // fetchMostViewed code
-    Projectview.find(
-      {order: 'viewCount DESC', limit: 10, include: ['solveitproject']},
-      function(err, projects) {
-        cb(null, projects);
-      }
-    );
-
-    Projectview.remoteMethod('fetchMostViewed', {
-      description: 'fetch most viewed projects',
-      http: {
-        verb: 'get',
-        path: '/mostViewed',
-      },
-      returns: {
-        type: 'object',
-        root: true,
-      },
-    });
-  };
 };
