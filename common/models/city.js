68c916c4585b9f9754e35739933251c03640d6f4
Nathan <nathantsegaye839@gmail.com>
2019-11-08 07:26:27 -0400

    validate uniqueness of city name

--- common/models/city.js
+++ common/models/city.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(City) {
-  
+  City.validatesUniquenessOf('name', {ignoreCase: 'ignoreCase'});
   //  disable delete end point
   City.disableRemoteMethod("deleteById", true);
   City.disableRemoteMethod("destroyById", true);
