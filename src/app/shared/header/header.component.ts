a7742382f1f580ad832e35bfd44db2bbfd5da468
Nathan <nathantsegaye839@gmail.com>
2019-11-26 06:46:58 -0400

    add notification to header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,3 +1,5 @@
+import { CommonService } from './../services/common.service';
+import { SharedService } from './../services/shared.service';
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../Auth/services/auth.service';
 import {Router} from '@angular/router';
@@ -8,10 +10,33 @@ import {Router} from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(public authService: AuthService, public router: Router) { }
+  userId;
+  notificationList = []
+  constructor(public authService: AuthService, 
+              public router: Router,
+              private commonService: CommonService) { }
 
   ngOnInit() {
+    this.userId = this.authService.getUserId()
+    console.log(this.userId)
+    if(this.userId){
+      this.getNotifications(this.userId)
+    }
+  }
+
+  getNotifications(userId){
+    this.commonService.getNotification(userId)
+    .subscribe((res =>{
+      console.log(res)
+      this.notificationList = res;
+    }))
+  }
+
+  deleteNotification(notification){
+    this.commonService.deleteNotification(notification)
+    .subscribe(res =>{
+      this.ngOnInit()
+    })
   }
 
 }
