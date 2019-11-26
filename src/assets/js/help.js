98f445692e69f6e6d36ab4d8eb3119825032c5f4
Nathan <nathantsegaye839@gmail.com>
2019-11-26 06:45:20 -0400

    static files

---
+++ src/assets/js/help.js
@@ -0,0 +1,95 @@
+/* reaveal on scroll section*/
+ScrollReveal().reveal('#section1', {
+    reset: true,
+    delay: 150,
+    distance: '100px',
+    origin: 'left'
+});
+ScrollReveal().reveal('#section2', {
+    reset: true,
+    delay: 250,
+    distance: '50px',
+    scale: 0.85
+});
+ScrollReveal().reveal('#section3', {
+    reset: true,
+    delay: 350,
+    distance: '50px',
+    scale: 0.85
+});
+ScrollReveal().reveal('#section4', {
+    reset: true,
+    delay: 500,
+    scale: 0.85
+});
+ScrollReveal().reveal('#section5', {
+    reset: true,
+    origin: 'left'
+});
+
+/* typewriter animation */
+var i = 0;
+var txt = 'Hey, Do you need help?';
+var speed = 50;
+
+function typeWriter() {
+    console.log("kokO")
+    if (i < txt.length) {
+        document.getElementById("help-text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter();
+
+/* smooth scrolling */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+
+        if (e.target.classList.contains('side')) {
+            document.querySelector('.help-sidebar').classList.add('hidden-sidebar');
+        }
+
+    });
+});
+
+/* Add a background to the nav bar on scroll */
+var scrollpos = window.scrollY;
+var header = document.querySelector(".hh-nav");
+var header_height = header.offsetHeight;
+var add_class_on_scroll = () => header.classList.add("nav-gradient-background");
+var remove_class_on_scroll = () => header.classList.remove("nav-gradient-background");
+window.addEventListener('scroll', function () {
+    scrollpos = window.scrollY;
+    if (scrollpos >= header_height) {
+        add_class_on_scroll();
+        document.querySelector('.page-title').classList.remove('invisible');
+    } else {
+        remove_class_on_scroll();
+        document.querySelector('.page-title').classList.add('invisible');
+    }
+});
+
+/* sidebar reveal */
+document.getElementById('showSidebar').addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    document.querySelector('.help-sidebar').classList.toggle('hidden-sidebar');
+});
+
+document.querySelector('.help-sidebar').addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+});
+
+document.body.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('help-sidebar')) {
+        document.querySelector('.help-sidebar').classList.add('hidden-sidebar');
+    }
+});
