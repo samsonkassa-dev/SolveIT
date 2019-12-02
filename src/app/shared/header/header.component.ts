b8a26b5baf01e0b757da61fe24edbe83d6722918
Nathan <nathantsegaye839@gmail.com>
2019-12-02 10:35:01 +0300

    delete notfication method added

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -14,7 +14,7 @@ export class HeaderComponent implements OnInit {
   notificationList = []
   constructor(public authService: AuthService, 
               public router: Router,
-              private commonService: CommonService) { }
+              private commonService: CommonService,) { }
 
   ngOnInit() {
     this.userId = this.authService.getUserId()
@@ -23,16 +23,20 @@ export class HeaderComponent implements OnInit {
       this.getNotifications(this.userId)
     }
   }
-
+  getUserProfile(){
+    // [routerLink]="['/userProfile', authService.getUserId()]"
+    console.log(this.authService.getUserId())
+    this.router.navigate(['dashboard','userProfile', this.authService.getUserId()])
+  } 
   getNotifications(userId){
     this.commonService.getNotification(userId)
     .subscribe((res =>{
-      console.log(res)
       this.notificationList = res;
     }))
   }
 
   deleteNotification(notification){
+    console.log(notification)
     this.commonService.deleteNotification(notification)
     .subscribe(res =>{
       this.ngOnInit()
