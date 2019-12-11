0e28d822c107c5f772718541ddd38654c690f552
Nathan <nathantsegaye839@gmail.com>
2019-12-11 20:36:52 +0300

    archive sponsors

--- src/app/sponsors/sponsors-page/sponsors-page.component.ts
+++ src/app/sponsors/sponsors-page/sponsors-page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-sponsors-page',
   templateUrl: './sponsors-page.component.html',
   styleUrls: ['./sponsors-page.component.css']
 })
 export class SponsorsPageComponent implements OnInit {
-
+  whatYear : any = "2019";
+  showArchivedPartner = false;
+  options = [
+    "archived",
+    "2019"
+  ]
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  toggleArchived(){
+    this.showArchivedPartner = !this.showArchivedPartner
   }
 
 }
