ed8852d17d92e3c51f9af6051e69fd3e0b356e51
Nathan <nathantsegaye839@gmail.com>
2020-02-03 13:21:01 +0300

    how to register link added

--- src/app/help/help-page/help-page.component.ts
+++ src/app/help/help-page/help-page.component.ts
@@ -1,26 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-help-page',
-  templateUrl: './help-page.component.html',
-  styleUrls: ['./help-page.component.css']
+  selector: "app-help-page",
+  templateUrl: "./help-page.component.html",
+  styleUrls: ["./help-page.component.css"]
 })
 export class HelpPageComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    setTimeout (() => {
-     
+    setTimeout(() => {
       this.loadScript("help.js");
-   }, 1000);
+    }, 1000);
   }
-  public loadScript(jsFile){
-    let body=<HTMLDivElement> document.body;
-    let script=document.createElement('script');
-    script.src='../../../assets/js/'+jsFile;
-    script.async=true;
-    script.defer=true;
+  goTo(link) {
+    window.open(link, "_blank");
+  }
+  public loadScript(jsFile) {
+    let body = <HTMLDivElement>document.body;
+    let script = document.createElement("script");
+    script.src = "../../../assets/js/" + jsFile;
+    script.async = true;
+    script.defer = true;
     body.appendChild(script);
   }
 }
