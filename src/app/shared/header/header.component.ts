7f2293ebaa48ce0d7860c72474c7d1235868a5b8
Nathan <nathantsegaye839@gmail.com>
2020-02-03 13:23:49 +0300

    fix notification

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,46 +1,56 @@
-import { CommonService } from './../services/common.service';
-import { SharedService } from './../services/shared.service';
-import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../../Auth/services/auth.service';
-import {Router} from '@angular/router';
-
+import { CommonService } from "./../services/common.service";
+import { SharedService } from "./../services/shared.service";
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../Auth/services/auth.service";
+import { Router } from "@angular/router";
+declare var $: any;
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
   userId;
-  notificationList = []
-  constructor(public authService: AuthService, 
-              public router: Router,
-              private commonService: CommonService,) { }
+  notificationList = [];
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    private commonService: CommonService
+  ) {}
 
   ngOnInit() {
-    this.userId = this.authService.getUserId()
+    this.userId = this.authService.getUserId();
+    $("a").on("click", function(e) {
+      e.preventDefault();
+      if (document.querySelector(".nav-transparent")) {
+        document.querySelector(".nav-transparent").classList.remove("open");
+        document.querySelector(".navbar-collapse").classList.remove("in");
+      }
+    });
     //console.log(this.userId)
-    if(this.userId){
-      this.getNotifications(this.userId)
+    if (this.userId) {
+      this.getNotifications(this.userId);
     }
   }
-  getUserProfile(){
+  getUserProfile() {
     // [routerLink]="['/userProfile', authService.getUserId()]"
     //console.log(this.authService.getUserId())
-    this.router.navigate(['dashboard','userProfile', this.authService.getUserId()])
-  } 
-  getNotifications(userId){
-    this.commonService.getNotification(userId)
-    .subscribe((res =>{
+    this.router.navigate([
+      "dashboard",
+      "userProfile",
+      this.authService.getUserId()
+    ]);
+  }
+  getNotifications(userId) {
+    this.commonService.getNotification(userId).subscribe(res => {
       this.notificationList = res;
-    }))
+    });
   }
 
-  deleteNotification(notification){
+  deleteNotification(notification) {
     //console.log(notification)
-    this.commonService.deleteNotification(notification)
-    .subscribe(res =>{
-      this.ngOnInit()
-    })
+    this.commonService.deleteNotification(notification).subscribe(res => {
+      this.ngOnInit();
+    });
   }
-
 }
