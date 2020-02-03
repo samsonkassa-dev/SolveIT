f22d28740f1d7fe2360f30d3bb1b32e3eeeef92d
Nathan <nathantsegaye839@gmail.com>
2020-02-03 13:21:52 +0300

    add non existing member

--- src/app/project/memberList/memberList.component.ts
+++ src/app/project/memberList/memberList.component.ts
@@ -1,7 +1,8 @@
+import { FormGroup, FormBuilder } from "@angular/forms";
 import { Component, Input, OnInit } from "@angular/core";
 import { ProjectService } from "../project.service";
-import { AuthService } from '../../Auth/services/auth.service';
-import { Router } from '@angular/router';
+import { AuthService } from "../../Auth/services/auth.service";
+import { Router } from "@angular/router";
 import { SharedService } from "../../shared/services/shared.service";
 
 declare var $: any;
@@ -19,22 +20,62 @@ export class ProjectMemberList implements OnInit {
   public keyword = "";
   public page = 1;
   memberToRemove = null;
+  addMemberForm: FormGroup;
 
-  constructor(public service: ProjectService, private sharedService: SharedService, public authService: AuthService, private router: Router) {}
-
+  constructor(
+    public fb: FormBuilder,
+    public service: ProjectService,
+    private sharedService: SharedService,
+    public authService: AuthService,
+    private router: Router
+  ) {
+    this.addMemberForm = this.fb.group({
+      firstName: "",
+      middleName: ""
+    });
+  }
+  addMember(form) {
+    if (!this.project.unregisteredMembers) {
+      this.project.unregisteredMembers = [];
+      this.project.unregisteredMembers.push(form);
+    } else {
+      this.project.unregisteredMembers.push(form);
+    }
+    this.service.updateProject(this.project.id, this.project).subscribe(res => {
+      this.sharedService.addToast(
+        "Success",
+        "Member Added successfully!",
+        "success"
+      );
+    });
+  }
   ngOnInit() {
     this.getMembers();
   }
 
   showUserProfile(member) {
-    if (this.authService.isAdmin() || this.authService.isSolveitManager() || this.authService.isSolveitTeam()) {
-      this.router.navigate(['/dashboard/userProfile', member.id]);
+    if (
+      this.authService.isAdmin() ||
+      this.authService.isSolveitManager() ||
+      this.authService.isSolveitTeam()
+    ) {
+      this.router.navigate(["/dashboard/userProfile", member.id]);
     }
   }
 
   getMembers() {
     this.service.getMembers(this.project.id).subscribe(res => {
       this.members = res;
+      this.members = this.members.filter(member => {
+        if (member.role) {
+          return member.role.name == "solve-it-participants";
+        }
+        return false;
+      });
+      if (this.project.unregisteredMembers) {
+        this.members = [...this.members, ...this.project.unregisteredMembers];
+      }
+
       this.membersBackup = res;
     });
   }
@@ -53,9 +94,9 @@ export class ProjectMemberList implements OnInit {
     const temp = {
       userId: member.id,
       projectId: this.project.id
-    }
-    this.service.removeProjectMember({member: temp})
-      .subscribe(res => {
+    };
+    this.service.removeProjectMember({ member: temp }).subscribe(
+      res => {
         this.members.splice(this.members.indexOf(member), 1);
         this.toggleModal(null);
         this.sharedService.addToast(
@@ -63,17 +104,16 @@ export class ProjectMemberList implements OnInit {
           "Member removed successfully!",
           "success"
         );
-      }, error => {
+      },
+      error => {
         //console.log(error);
         this.sharedService.addToast("", "Error occurred!", "error");
-      });
-
-    
+      }
+    );
   }
 
   toggleModal(member) {
     this.memberToRemove = member;
-    $('#removeMemberModal').modal('toggle');
+    $("#removeMemberModal").modal("toggle");
   }
-
 }
