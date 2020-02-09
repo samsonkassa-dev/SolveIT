881de03799fe59acba47e308c9dd47dbed8f7853
Nathan <nathantsegaye839@gmail.com>
2020-02-09 11:38:59 +0300

    add stat routes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,30 +1,28 @@
-import { JudgeModule } from './judge/judge.module';
-import { SolveItTeamMemberModule } from './solveit-team-member/solveit-team-member.module';
-import {NgModule} from '@angular/core';
-import {HeaderComponent} from '../shared/header/header.component';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
-import {ToastyModule, ToastyService} from 'ng2-toasty';
-import {ApiService} from '../shared/services/api.service';
-import {SharedService} from '../shared/services/shared.service';
-import {SharedModule} from '../shared/shared.module';
-import {UserManagementModule} from '../userManagement/userManagement.module';
-import {DashboardComponent} from './dashboard.component';
-import {CompetitionModule} from '../competition/competition.module';
-import {CategoryModule} from './category/category.module';
-import {TagModule} from './tag/tag.module';
-import {AdminGuardService} from '../Auth/services/admin-guard.service';
-import { CityModule } from './city/city.module';
-import { DASHBOARD_ROUTES } from './dashboard.routes';
-import { ReviewDiscussionComponent } from './reviewDiscussion/reviewDiscussion.component';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { AttendanceModule } from './attendance/attendance.module';
+import { StatModule } from "./stat/stat.module";
+import { JudgeModule } from "./judge/judge.module";
+import { SolveItTeamMemberModule } from "./solveit-team-member/solveit-team-member.module";
+import { NgModule } from "@angular/core";
+import { HeaderComponent } from "../shared/header/header.component";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { ToastyModule, ToastyService } from "ng2-toasty";
+import { ApiService } from "../shared/services/api.service";
+import { SharedService } from "../shared/services/shared.service";
+import { SharedModule } from "../shared/shared.module";
+import { UserManagementModule } from "../userManagement/userManagement.module";
+import { DashboardComponent } from "./dashboard.component";
+import { CompetitionModule } from "../competition/competition.module";
+import { CategoryModule } from "./category/category.module";
+import { TagModule } from "./tag/tag.module";
+import { AdminGuardService } from "../Auth/services/admin-guard.service";
+import { CityModule } from "./city/city.module";
+import { DASHBOARD_ROUTES } from "./dashboard.routes";
+import { ReviewDiscussionComponent } from "./reviewDiscussion/reviewDiscussion.component";
+import { NgxPaginationModule } from "ngx-pagination";
+import { AttendanceModule } from "./attendance/attendance.module";
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    ReviewDiscussionComponent
-  ],
+  declarations: [DashboardComponent, ReviewDiscussionComponent],
   imports: [
     RouterModule.forChild(DASHBOARD_ROUTES),
     CommonModule,
@@ -35,6 +33,7 @@ import { AttendanceModule } from './attendance/attendance.module';
     CompetitionModule,
     CategoryModule,
     TagModule,
+    StatModule,
     CityModule,
     SolveItTeamMemberModule,
     AttendanceModule,
@@ -42,6 +41,6 @@ import { AttendanceModule } from './attendance/attendance.module';
     NgxPaginationModule
   ],
   providers: [ApiService, ToastyService, SharedService, AdminGuardService],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent]
 })
-export class DashboardModule { }
+export class DashboardModule {}
--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -1,9 +1,10 @@
-import { JudgeProfileComponent } from './judge/judge-profile/judge-profile.component';
-import { JudgeListComponent } from './judge/judge-list/judge-list.component';
-import { AttendanceDetailComponent } from './attendance/attendance-detail/attendance-detail.component';
-import { SolveItTeamMemberEditComponent } from './solveit-team-member/solve-it-team-member-edit/solve-it-team-member-edit.component';
-import { SolveItTeamMemberAddComponent } from './solveit-team-member/solve-it-team-member-add/solve-it-team-member-add.component';
-import { SolveItTeamMemberListComponent } from './solveit-team-member/solve-it-team-member-list/solve-it-team-member-list.component';
+import { StatListComponent } from "./stat/stat-list/stat-list.component";
+import { JudgeProfileComponent } from "./judge/judge-profile/judge-profile.component";
+import { JudgeListComponent } from "./judge/judge-list/judge-list.component";
+import { AttendanceDetailComponent } from "./attendance/attendance-detail/attendance-detail.component";
+import { SolveItTeamMemberEditComponent } from "./solveit-team-member/solve-it-team-member-edit/solve-it-team-member-edit.component";
+import { SolveItTeamMemberAddComponent } from "./solveit-team-member/solve-it-team-member-add/solve-it-team-member-add.component";
+import { SolveItTeamMemberListComponent } from "./solveit-team-member/solve-it-team-member-list/solve-it-team-member-list.component";
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "./dashboard.component";
 import { DashboardGuardService } from "../Auth/services/dashboard-guard.service";
@@ -20,7 +21,7 @@ import { ExportDataComponent } from "../userManagement/exportData/exportData.com
 import { AdminGuardService } from "../Auth/services/admin-guard.service";
 import { SolveitMgmtGuardService } from "../Auth/services/solveit-mgmt-guard.service";
 import { UserModuleGuardService } from "../Auth/services/userModuleGuard.service";
-import { AttendanceListComponent } from './attendance/attendance-list/attendance-list.component';
+import { AttendanceListComponent } from "./attendance/attendance-list/attendance-list.component";
 
 export const DASHBOARD_ROUTES: Routes = [
   {
@@ -48,14 +49,21 @@ export const DASHBOARD_ROUTES: Routes = [
       { path: "categories", component: CategoriesListComponent },
       { path: "tags", component: TagManagmentComponent },
       { path: "review-discussion", component: ReviewDiscussionComponent },
+      { path: "stats", component: StatListComponent },
       { path: "cities", component: CityListComponent },
       { path: "judges", component: JudgeListComponent },
-      { path: "judges/:judge_id", component: JudgeProfileComponent},
+      { path: "judges/:judge_id", component: JudgeProfileComponent },
       { path: "attendance", component: AttendanceListComponent },
-      { path: "attendance/:attendance_id", component: AttendanceDetailComponent },
+      {
+        path: "attendance/:attendance_id",
+        component: AttendanceDetailComponent
+      },
       { path: "solveit-team", component: SolveItTeamMemberListComponent },
       { path: "solveit-team/add", component: SolveItTeamMemberAddComponent },
-      { path: "solveit-team/edit/:member_id", component: SolveItTeamMemberEditComponent },
+      {
+        path: "solveit-team/edit/:member_id",
+        component: SolveItTeamMemberEditComponent
+      },
       { path: "export-data", component: ExportDataComponent }
     ]
   },
@@ -64,6 +72,5 @@ export const DASHBOARD_ROUTES: Routes = [
     component: CompetitionProjectsComponent,
     canActivate: [DashboardGuardService]
   },
-  { path: "userProfile/:userId", component: UserProfileComponent },
-
+  { path: "userProfile/:userId", component: UserProfileComponent }
 ];
