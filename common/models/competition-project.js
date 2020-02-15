adca02b7404fcd80eac9b7a00d0aa5b58be2a9bc
Nathan <nathantsegaye839@gmail.com>
2020-02-15 14:01:35 +0300

    disable remote method

--- common/models/competition-project.js
+++ common/models/competition-project.js
@@ -2,9 +2,9 @@
 
 module.exports = function(Competitionproject) {
   //  disable delete end point
-  // Competitionproject.disableRemoteMethod('deleteById', true);
-  // Competitionproject.disableRemoteMethod('destroyById', true);
-  // Competitionproject.disableRemoteMethod('removeById', true);
+  Competitionproject.disableRemoteMethod("deleteById", true);
+  Competitionproject.disableRemoteMethod("destroyById", true);
+  Competitionproject.disableRemoteMethod("removeById", true);
 
   Competitionproject.enroll = function(
     competitionId,
