fffffff13a301de8e6f1626b7c7979533d0ba848
Nathan <nathantsegaye839@gmail.com>
2020-03-12 05:04:08 +0300

    get 2020 participant

--- common/models/attendance.js
+++ common/models/attendance.js
@@ -1,29 +1,39 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable padded-blocks */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable comma-dangle */
+/* eslint-disable camelcase */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable semi */
+/* eslint-disable indent */
 'use strict';
 
 module.exports = function (Attendance) {
-    Attendance.observe('before save', (ctx, next) =>{
+    Attendance.observe('before save', (ctx, next) => {
 
-        if(ctx.instance){
+        if (ctx.instance) {
             let cityIds = ctx.instance.cityIds
             let Useraccount = Attendance.app.models.UserAccount
             let { IcogRole } = Useraccount.app.models;
             console.log(cityIds)
             IcogRole.findOne({
                 where: { name: "solve-it-participants" }
-            },(error, role) =>{
-                if(error){
+            }, (error, role) => {
+                if (error) {
                     next(error)
-                }else{
-                    Useraccount.find({where : {cityId : {inq: cityIds}, roleId: role.id}}, (error, users) =>{
-                        if(error){
+                } else {
+                    let now = new Date(2020, 0, 1);
+
+                    Useraccount.find({ where: { cityId: { inq: cityIds }, roleId: role.id, created: { gte: now }, } }, (error, users) => {
+                        if (error) {
                             next(error)
-                        }else{
+                        } else {
                             console.log(users.length)
                             let participants = []
                             users.forEach(user => {
                                 participants.push({
-                                    participant : user,
-                                    is_present: false
+                                    participant: user,
+                                    is_present: false,
                                 })
                             });
                             ctx.instance.participants = participants
@@ -32,11 +42,11 @@ module.exports = function (Attendance) {
                     })
                 }
             })
-            
-        }else{
+
+        } else {
             next()
         }
-        
-        
+
+
     })
 };
