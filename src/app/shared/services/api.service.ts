2250b0a01c6be3fa48f0f81a04ef7b127654daca
Nathan <nathantsegaye839@gmail.com>
2020-04-28 07:39:06 +0300

    refactor spacing

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,58 +1,61 @@
-import {Injectable} from '@angular/core';
-import {Headers, Http, Response, ResponseContentType} from '@angular/http';
-import {configs} from '../../app.config';
-import 'rxjs/add/operator/map'
-import {Observable} from '../../../../node_modules/rxjs/Observable';
-
+import { Injectable } from "@angular/core";
+import { Headers, Http, Response, ResponseContentType } from "@angular/http";
+import { configs } from "../../app.config";
+import "rxjs/add/operator/map";
+import { Observable } from "../../../../node_modules/rxjs/Observable";
 @Injectable()
 export class ApiService {
-
   public ROOT_PATH = configs.rootUrl;
-  TOKEN_KEY = 'access_token';
+  TOKEN_KEY = "access_token";
   headers: Headers = new Headers({
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
+    "Content-Type": "application/json",
+    Accept: "application/json"
   });
 
-  constructor(public http: Http) {
-  }
+  constructor(public http: Http) {}
 
   public get(path: string): Observable<any> {
     this.setHeaders(this.TOKEN_KEY);
-    return this.http.get(`${this.ROOT_PATH}${path}`, {headers: this.headers})
+    return this.http
+      .get(`${this.ROOT_PATH}${path}`, { headers: this.headers })
       .map((res: Response) => res.json());
-
   }
 
   public post(path: string, body): Observable<any> {
     this.setHeaders(this.TOKEN_KEY);
-    return this.http.post(`${this.ROOT_PATH}${path}`, body, {headers: this.headers})
+    return this.http
+      .post(`${this.ROOT_PATH}${path}`, body, { headers: this.headers })
       .map((res: Response) => res.json());
   }
 
   public put(path: string, body): Observable<any> {
     this.setHeaders(this.TOKEN_KEY);
-    return this.http.put(`${this.ROOT_PATH}${path}`, body, {headers: this.headers})
+    return this.http
+      .put(`${this.ROOT_PATH}${path}`, body, { headers: this.headers })
       .map((res: Response) => res.json());
   }
 
   public downloadExcel(path: string, body) {
     this.setHeaders(window.localStorage.getItem(this.TOKEN_KEY));
-    return this.http.post(path, body,{
-        headers:this.headers,
+    return this.http
+      .post(path, body, {
+        headers: this.headers,
         responseType: ResponseContentType.Blob
-    }).map(res => new Blob([res.blob()],{ type: 'application/vnd.ms-excel' }));
-}
+      })
+      .map(res => new Blob([res.blob()], { type: "application/vnd.ms-excel" }));
+  }
 
   public delete(path: string): Observable<any> {
     this.setHeaders(this.TOKEN_KEY);
-    return this.http.delete(`${this.ROOT_PATH}${path}`, {headers: this.headers})
+    return this.http
+      .delete(`${this.ROOT_PATH}${path}`, { headers: this.headers })
       .map((res: Response) => res.json());
   }
 
   public patch(path: string, body: any): Observable<any> {
     this.setHeaders(this.TOKEN_KEY);
-    return this.http.patch(`${this.ROOT_PATH}${path}`, body, {headers: this.headers})
+    return this.http
+      .patch(`${this.ROOT_PATH}${path}`, body, { headers: this.headers })
       .map((res: Response) => res.json());
   }
 
@@ -61,7 +64,7 @@ export class ApiService {
     if (temp != null) {
       try {
         const session = JSON.parse(temp);
-        this.headers.set('Authorization', session.id);
+        this.headers.set("Authorization", session.id);
       } catch (e) {
         return;
       }
@@ -70,15 +73,15 @@ export class ApiService {
 
   public download(path: string, fileName: string) {
     const headers = new Headers({
-        'Content-Type': 'application/json',
-        responseType: ResponseContentType.Blob
-      });
-    return this.http.get(`${this.ROOT_PATH}${path}`, {
+      "Content-Type": "application/json",
       responseType: ResponseContentType.Blob
-    })
+    });
+    return this.http
+      .get(`${this.ROOT_PATH}${path}`, {
+        responseType: ResponseContentType.Blob
+      })
       .map(res => {
-        return {data: res.blob(), fileName: fileName};
+        return { data: res.blob(), fileName: fileName };
       });
   }
-
 }
