0d5fbaf536c96babc4006057b2fe569bfaeaa0cc
Nathan <nathantsegaye839@gmail.com>
2020-04-28 07:45:06 +0300

    restrict access to upgrade level

--- common/models/solveit-project.js
+++ common/models/solveit-project.js
@@ -1,29 +1,45 @@
-'use strict';
-
-module.exports = function(Solveitproject) {
+"use strict";
+const ValidateUtils = require("../utils/roleValidate");
+module.exports = function (Solveitproject) {
   //  disable delete end point
-  Solveitproject.disableRemoteMethod('deleteById', true);
-  Solveitproject.disableRemoteMethod('destroyById', true);
-  Solveitproject.disableRemoteMethod('removeById', true);
+  Solveitproject.disableRemoteMethod("deleteById", true);
+  Solveitproject.disableRemoteMethod("destroyById", true);
+  Solveitproject.disableRemoteMethod("removeById", true);
 
+  Solveitproject.observe("before save", async (ctx, next) => {
+    let { UserAccount } = Solveitproject.app.models;
+    let token = ctx.options && ctx.options.accessToken;
+    if (
+      ctx.currentInstance !== undefined &&
+      ctx.data.level &&
+      ctx.currentInstance.level &&
+      ctx.data.level != ctx.currentInstance.level
+    ) {
+      try {
+        let res = await ValidateUtils.roleValidate(UserAccount, token.userId);
+      } catch (error) {
+        next(new Error(error));
+      }
+    }
+  });
   // search projects
-  Solveitproject.search = function(keyword, cb) {
+  Solveitproject.search = function (keyword, cb) {
     // search code
   };
 
-  Solveitproject.remoteMethod('search', {
-    description: 'Search projects',
+  Solveitproject.remoteMethod("search", {
+    description: "Search projects",
     accepts: {
-      arg: 'keyword',
-      type: 'string',
+      arg: "keyword",
+      type: "string",
       require: true,
     },
     http: {
-      verb: 'post',
-      path: '/search',
+      verb: "post",
+      path: "/search",
     },
     returns: {
-      type: 'object',
+      type: "object",
       root: true,
     },
   });
