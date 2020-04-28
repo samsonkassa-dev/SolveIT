5716df0df99accfb058863ac8469a72ad8bb59ab
Nathan <nathantsegaye839@gmail.com>
2020-04-28 07:43:46 +0300

    add activity score and compute level if score is passable

--- common/models/progress-report.js
+++ common/models/progress-report.js
@@ -1,11 +1,89 @@
 "use strict";
 const NotificationUtils = require("../utils/notificationUtil");
+const ValidateUtils = require("../utils/roleValidate");
 
 module.exports = function (Progressreport) {
   //  disable delete end point
-  Progressreport.disableRemoteMethod("deleteById", true);
-  Progressreport.disableRemoteMethod("destroyById", true);
-  Progressreport.disableRemoteMethod("removeById", true);
+  // Progressreport.disableRemoteMethod("deleteById", true);
+  // Progressreport.disableRemoteMethod("destroyById", true);
+  // Progressreport.disableRemoteMethod("removeById", true);
+  Progressreport.observe("before save", async (ctx, next) => {
+    let { UserAccount, Activity, Solveitproject } = Progressreport.app.models;
+    let token = ctx.options && ctx.options.accessToken;
+    if (
+      ctx.currentInstance !== undefined &&
+      ctx.data.activity_score != ctx.currentInstance.activity_score
+    ) {
+      try {
+        let res = await ValidateUtils.roleValidate(UserAccount, token.userId);
+        const project = await Solveitproject.findOne({
+          where: { id: ctx.data.projectId },
+          include: {
+            relation: "reports",
+            scope: {
+              where: { type: "activity" },
+              include: ["activity"],
+            },
+          },
+        });
+        let proj = project.toJSON();
+        const activities = await Activity.find({
+          where: { level: project.level },
+        });
+        const current_activity = await Activity.findOne({
+          where: { id: ctx.data.activityId },
+        });
+        //update the project level only when the score of the activity at the project level is updated
+        if (activities.length > 0 && current_activity.level == project.level) {
+          const reports = proj.reports;
+          //check if the have all activity have been submitted
+          let levelActivityIds = activities.map((activity) =>
+            JSON.stringify(activity.id.toString())
+          );
+          let projectActivityIds = reports.map((report) =>
+            JSON.stringify(report.activityId)
+          );
+          let allFound = true;
+          levelActivityIds.forEach((activity) => {
+            if (projectActivityIds.indexOf(activity) < 0) {
+              allFound = false;
+            }
+          });
+          console.log("All Activities Submitted " + allFound);
+          if (allFound) {
+            let allPass = true;
+            reports.forEach((report) => {
+              let act_score = report.activity_score;
+              let passing_grade = report.activity.passing_grade;
+              if (act_score < passing_grade) {
+                allPass = false;
+              }
+            });
+            console.log("Passed All" + allPass);
+            if (allPass) {
+              project.updateAttributes({ level: project.level + 1 }, function (
+                err,
+                old
+              ) {
+                if (err) {
+                  console.log("eErrrrrrrr");
+                  console.log(err);
+                } else {
+                  console.log("Updated OLD");
+                }
+              });
+            }
+          }
+        } else {
+          console.log("lese");
+        }
+      } catch (error) {
+        next(new Error(error));
+      }
+    } else {
+      console.log("They Want ");
+    }
+  });
 
   Progressreport.afterRemote("create", async (ctx, unused, next) => {
     const {
@@ -53,7 +131,6 @@ module.exports = function (Progressreport) {
           }
         });
         const message = `Dear ${projectCity.user.firstName}, ${reportUploader.firstName} uploaded progress report on "${project.title}".`;
-        console.log(projectCity);
         let newNotification = {
           projectId: project.id,
           userId: projectCity.userId,
