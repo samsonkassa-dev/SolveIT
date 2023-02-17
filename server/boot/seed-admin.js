ccf2c5e3e83209db4b2b9e5e15148dede2b19e8b
AlAswaad99 <dannyboy9917@gmail.com>
2023-02-17 14:25:37 +0100

    migrated to lb3, admin-regions-directories boot script fixes

--- server/boot/seed-admin.js
+++ server/boot/seed-admin.js
@@ -1,22 +1,22 @@
-'use strict';
+// 'use strict';
 
-module.exports = function(app) {
-  const { IcogRole } = app.models;
-  const { UserAccount } = app.models;
+// module.exports = function(app) {
+//   const { IcogRole } = app.models;
+//   const { UserAccount } = app.models;
 
-  let createAdmin = async () =>  {
-    let role = await IcogRole.findOne({"where": {"name": "admin"}});
-    let admin ={
-      "email": "admin@solvei.com",
-      "password": "ssolveitadminpass",
-      "roleId": role ? role.id : "63d687749ad27704efae59c3",
-      "username": "admin",
-      "emailVerified": true
-    }
-    return UserAccount.findOrCreate({"where": {"email": "admin@solveit.com"}}, admin)
-    .then(instance => Promise.resolve(instance))
-    .catch(err => Promise.reject(err));
-  };
+//   let createAdmin = async () =>  {
+//     let role = await IcogRole.findOne({"where": {"name": "admin"}});
+//     let admin ={
+//       "email": "admin@solveit.com",
+//       "password": "solveitadminpass",
+//       "roleId": role.id || "63d687749ad27704efae59c3",
+//       "username": "admin",
+//       "emailVerified": true
+//     }
+//     return UserAccount.findOrCreate({"where": {"email": "admin@solveit.com"}}, admin)
+//     .then(instance => Promise.resolve(instance))
+//     .catch(err => Promise.reject(err));
+//   };
 
-  createAdmin();
-};
+//   createAdmin();
+// };
