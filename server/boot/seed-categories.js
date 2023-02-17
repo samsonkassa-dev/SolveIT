67d1b7847f07ce1d1d9e0ec818792e12fa84f37d
AlAswaad99 <dannyboy9917@gmail.com>
2023-02-17 14:25:37 +0100

    migrated to lb3, admin-regions-directories boot script fixes

--- server/boot/seed-categories.js
+++ server/boot/seed-categories.js
@@ -1,8 +1,10 @@
 'use strict';
+var fs = require('fs');
 
-module.exports = function(app) {
+module.exports = function (app) {
   const { forumCategory } = app.models;
   const { tag } = app.models;
+  const { Region } = app.models;
 
   const categories = [
     "Python",
@@ -13,6 +15,31 @@ module.exports = function(app) {
     "Others"
   ];
 
+  const regions = [
+    "Addis Ababa",
+    "Dire Dawa",
+    "Tigray",
+    "Afar",
+    "Amhara",
+    "Oromia",
+    "Somali",
+    "Benishangul-Gumuz",
+    "SNNPR",
+    "Gambella",
+    "Harari",
+  ];
+
+  const directories = [
+    "proposals",
+    "resources",
+    "reports",
+    "news",
+    "discussions",
+    "winner-thumbinals",
+    "solveit-team"
+
+  ]
+
   const tags = [
     "PHP",
     "OOP",
@@ -21,31 +48,62 @@ module.exports = function(app) {
     "Deep Learning"
   ];
 
-  let populateCategories = async () =>  {
+  const populateCategories = async () => {
     for (let i = 0; i < categories.length; i++) {
-      forumCategory.findOrCreate({where: {category: categories[i]}}, {category: categories[i]})
-      .then(res => {
-        console.log("categori item added.");
-      })
-      .catch(err => {
-        console.log(err);
-      })
+      forumCategory.findOrCreate({ where: { category: categories[i] } }, { category: categories[i] })
+        .then(res => {
+        })
+        .catch(err => {
+          console.log(err);
+        })
 
     }
+    console.log("Categories added.");
+
   };
 
-  let populateTags = async () => {
+  const populateTags = async () => {
     for (let i = 0; i < tags.length; i++) {
-      tag.findOrCreate({where: {name: tags[i]}}, {name: tags[i]})
-      .then(res => {
-        console.log("Tag item added.");
-      })
-      .catch(err => {
-        console.log(err);
-      })
+      tag.findOrCreate({ where: { name: tags[i] } }, { name: tags[i] })
+        .then(res => {
+        })
+        .catch(err => {
+          console.log(err);
+        })
     }
+    console.log("Tags added.");
+
   }
 
-  populateCategories();
-  populateTags();
+  const populateRegions = async () => {
+    for (let i = 0; i < regions.length; i++) {
+      await Region.findOrCreate({ where: { name: regions[i] } }, { name: regions[i] })
+        .then(res => {
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
+    console.log("Regions added.");
+  }
+
+  const createDirectories = async () => {
+    for (let index = 0; index < directories.length; index++) {
+      var dir = __dirname + '/../files/' + directories[index];
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+    }
+    console.log("Directories Created.");
+
+  }
+
+  async function createConstants() {
+    await populateCategories();
+    await populateTags();
+    await populateRegions();
+    await createDirectories();
+  }
+
+  createConstants();
 };
--- server/boot/seed-role.js
+++ server/boot/seed-role.js
@@ -1,74 +1,94 @@
 'use strict';
 
-module.exports = function(app) {
-  const { IcogRole } = app.models;
+module.exports = function (app) {
+	const { IcogRole } = app.models;
+	const { UserAccount } = app.models;
 
-    //  Seed iCog admin role
-  	const createIcogAminRole = /**async**/ () => {
+	//  Seed iCog admin role
+	const createIcogAminRole = /**async**/ () => {
 
-	    const role = { name: "admin", description: "Icog admin role" };
+		const role = { name: "admin", description: "Icog admin role" };
 
-	    return IcogRole.findOrCreate({ where: { name: "admin" } }, role)
-	      .then(createdRole => {})
-	      .then(instance => Promise.resolve(instance))
-	      .catch(err => Promise.reject(err));
-	  };
-
-  	// Seed SolveIT managment team role
-  	const createSolveItMgtRole = async () => {
-	    const role = { name: "solve-it-mgt", description: "SolveIT managment team role." };
-
-	    return IcogRole.findOrCreate({ where: { name: "solve-it-mgt" } }, role)
-	      .then(createdRole => {})
-	      .then(instance => Promise.resolve(instance))
-	      .catch(err => Promise.reject(err));
-	  };
-
-    // Seed SolveIT team role
-  	const createSolveItTeamRole = async () => {
-	    const role = { name: "solve-it-team", description: "SolveIT team role." };
-
-	    return IcogRole.findOrCreate({ where: { name: "solve-it-team" } }, role)
-	      .then(createdRole => {})
-	      .then(instance => Promise.resolve(instance))
-	      .catch(err => Promise.reject(err));
+		return IcogRole.findOrCreate({ where: { name: "admin" } }, role)
+			.then(createdRole => { })
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
 	};
 
-    // Seed SolveIT participants role
-  	const createSolveItParticipantsRole = async () => {
-	    const role = { name: "solve-it-participants", description: "SolveIT participants role." };
+	// Seed SolveIT managment team role
+	const createSolveItMgtRole = async () => {
+		const role = { name: "solve-it-mgt", description: "SolveIT managment team role." };
 
-	    return IcogRole.findOrCreate({ where: { name: "solve-it-participants" } }, role)
-	      .then(createdRole => {})
-	      .then(instance => Promise.resolve(instance))
-	      .catch(err => Promise.reject(err));
+		return IcogRole.findOrCreate({ where: { name: "solve-it-mgt" } }, role)
+			.then(createdRole => { })
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
+	};
+
+	// Seed SolveIT team role
+	const createSolveItTeamRole = async () => {
+		const role = { name: "solve-it-team", description: "SolveIT team role." };
+
+		return IcogRole.findOrCreate({ where: { name: "solve-it-team" } }, role)
+			.then(createdRole => { })
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
+	};
+
+	// Seed SolveIT participants role
+	const createSolveItParticipantsRole = async () => {
+		const role = { name: "solve-it-participants", description: "SolveIT participants role." };
+
+		return IcogRole.findOrCreate({ where: { name: "solve-it-participants" } }, role)
+			.then(createdRole => { })
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
 	};
 
 	// Seed SolveIT investor role
 	const createSolveItInvestorRole = async () => {
-			const role = { name: "solve-it-investor", description: "SolveIT investor role." };
+		const role = { name: "solve-it-investor", description: "SolveIT investor role." };
 
-			return IcogRole.findOrCreate({ where: { name: "solve-it-investor" } }, role)
-				.then(createdRole => {})
-				.then(instance => Promise.resolve(instance))
-				.catch(err => Promise.reject(err));
+		return IcogRole.findOrCreate({ where: { name: "solve-it-investor" } }, role)
+			.then(createdRole => { })
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
 	};
-// Seed SolveIT Judge role
-const createSolveItJudgeRole = async () => {
-	const role = { name: "solve-it-judge", description: "SolveIT Judge role." };
+	// Seed SolveIT Judge role
+	const createSolveItJudgeRole = async () => {
+		const role = { name: "solve-it-judge", description: "SolveIT Judge role." };
 
-	return IcogRole.findOrCreate({ where: { name: "solve-it-judge" } }, role)
-		.then(createdRole => {})
-		.then(instance => Promise.resolve(instance))
-		.catch(err => Promise.reject(err));
-};
+		return IcogRole.findOrCreate({ where: { name: "solve-it-judge" } }, role)
+			.then(createdRole => { })
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
+	};
 
 
-  createIcogAminRole();
-	createSolveItMgtRole();
-	createSolveItTeamRole();
-	createSolveItParticipantsRole();
-	createSolveItInvestorRole();
-	createSolveItJudgeRole();
+	// Create Admin User
+	const createAdmin = async () => {
+		let role = await IcogRole.findOne({ "where": { "name": "admin" } });
+		let admin = {
+			"email": "admin@solveit.com",
+			"password": "solveitadminpass",
+			"roleId": role.id,
+			"username": "admin",
+			"emailVerified": true
+		}
+		return UserAccount.findOrCreate({ "where": { "email": "admin@solveit.com" } }, admin)
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
+	};
 
+	async function createRoles() {
+		await createIcogAminRole();
+		await createSolveItMgtRole();
+		await createSolveItTeamRole();
+		await createSolveItParticipantsRole();
+		await createSolveItInvestorRole();
+		await createSolveItJudgeRole();
+		await createAdmin();
+	}
+
+	createRoles();
 };
