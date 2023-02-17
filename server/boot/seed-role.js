a6f84a3963f120af58bedd26ce5e913df0e04861
AlAswaad99 <dannyboy9917@gmail.com>
2023-02-17 14:25:37 +0100

    migrated to lb3, admin-regions-directories boot script fixes

--- server/boot/seed-role.js
+++ server/boot/seed-role.js
@@ -5,44 +5,44 @@ module.exports = function (app) {
 	const { UserAccount } = app.models;
 
 	//  Seed iCog admin role
-	const createIcogAminRole = /**async**/ () => {
+	const createIcogAminRole = async () => {
 
 		const role = { name: "admin", description: "Icog admin role" };
 
-	    return IcogRole.findOrCreate({ where: { name: "admin" } }, role)
-	      .then(createdRole => {})
-	      .then(instance => Promise.resolve(instance))
-	      .catch(err => Promise.reject(err));
-	  };
+		return IcogRole.findOrCreate({ where: { name: "admin" } }, role)
+			.then(createdRole => { })
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
+	};
 
-	  //  Seed SolveIT mentor role
-  	const createSolveItMentorRole = /**async**/ () => {
+	//  Seed SolveIT mentor role
+	const createSolveItMentorRole = async () => {
 
-	    const role = { name: "solve-it-mentor", description: "SolveIt mentor role" };
+		const role = { name: "solve-it-mentor", description: "SolveIt mentor role" };
 
-	    return IcogRole.findOrCreate({ where: { name: "solve-it-mentor" } }, role)
-	      .then(createdRole => {})
-	      .then(instance => Promise.resolve(instance))
-	      .catch(err => Promise.reject(err));
-	  };
-  	// Seed SolveIT managment team role
-  	const createSolveItMgtRole = async () => {
-	    const role = { name: "solve-it-mgt", description: "SolveIT managment team role." };
+		return IcogRole.findOrCreate({ where: { name: "solve-it-mentor" } }, role)
+			.then(createdRole => { })
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
+	};
+	// Seed SolveIT managment team role
+	const createSolveItMgtRole = async () => {
+		const role = { name: "solve-it-mgt", description: "SolveIT managment team role." };
 
-	    return IcogRole.findOrCreate({ where: { name: "solve-it-mgt" } }, role)
-	      .then(createdRole => {})
-	      .then(instance => Promise.resolve(instance))
-	      .catch(err => Promise.reject(err));
-	  };
+		return IcogRole.findOrCreate({ where: { name: "solve-it-mgt" } }, role)
+			.then(createdRole => { })
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
+	};
 
-    // Seed SolveIT team role
-  	const createSolveItTeamRole = async () => {
-	    const role = { name: "solve-it-team", description: "SolveIT team role." };
+	// Seed SolveIT team role
+	const createSolveItTeamRole = async () => {
+		const role = { name: "solve-it-team", description: "SolveIT team role." };
 
-	    return IcogRole.findOrCreate({ where: { name: "solve-it-team" } }, role)
-	      .then(createdRole => {})
-	      .then(instance => Promise.resolve(instance))
-	      .catch(err => Promise.reject(err));
+		return IcogRole.findOrCreate({ where: { name: "solve-it-team" } }, role)
+			.then(createdRole => { })
+			.then(instance => Promise.resolve(instance))
+			.catch(err => Promise.reject(err));
 	};
 
 	// // Seed SolveIT managment team role
@@ -93,6 +93,21 @@ module.exports = function (app) {
 			.then(instance => Promise.resolve(instance))
 			.catch(err => Promise.reject(err));
 	};
+
+
+	//   createIcogAminRole();
+	// 	createSolveItMgtRole();
+	// 	createSolveItTeamRole();
+	// 	createSolveItMentorRole();
+	// 	createSolveItParticipantsRole();
+	// 	createSolveItInvestorRole();
+	// 	createSolveItJudgeRole();
+
+	// 		return IcogRole.findOrCreate({ where: { name: "solve-it-judge" } }, role)
+	// 			.then(createdRole => { })
+	// 			.then(instance => Promise.resolve(instance))
+	// 			.catch(err => Promise.reject(err));
+	// 	};
 
 
 	// Create Admin User
